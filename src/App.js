8d8a18a2d06a8cd60480aedb41baac2540960674
Superdev0624 <superbaymax007@gmail.com>
2022-01-18 20:57:17 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactasdasdasdasdasdqwdaedddqwdqwdqwdqw
         </a>
       </header>
     </div>
