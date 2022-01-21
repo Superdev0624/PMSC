61bcf624a7de00ba50a7e39484ba0d05ab1f1db5
Superdev0624 <superbaymax007@gmail.com>
2022-01-21 09:15:55 -0500

    123

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 export default function Navbar(props) {
+  const [profile, setProfile] = useState('')
   return (
     <div>
       <nav className="w-full bg-gray-100 shadow">
-        <div className="flex justify-between h-16">
-          <div className="mr-5 h-full xl:flex items-center justify-end hidden">
+        {/* <div className="flex justify-between h-16"> */}
+          <div className="mr-5 xl:flex items-center justify-around hidden h-16">
             <div className="w-full h-full flex items-center">
               <div className="w-32 cursor-pointer">
                 <svg className="h-10 w-15 text-gray-600 hover:text-gray-400" viewBox="0 0 18 15" fill="currentColor" onClick={props.sidebarShow}>
@@ -14,6 +15,49 @@ export default function Navbar(props) {
                     clipRule="evenodd"
                   />
                 </svg>
+              </div>
+            </div>
+            <div className="w-full flex items-center pl-8 justify-end">
+              <div className="flex items-center relative cursor-pointer" onClick={() => setProfile(!profile)}>
+                <div className="rounded-full">
+                  {profile ? (
+                    <ul className="p-2 w-full border-r bg-white absolute rounded left-0 shadow mt-12 sm:mt-16 ">
+                      <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center">
+                        <div className="flex items-center">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width={18} height={18} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <circle cx={12} cy={7} r={4} />
+                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                          </svg>
+                          <span className="text-sm ml-2">My Profile</span>
+                        </div>
+                      </li>
+                      <li className="flex w-full justify-between text-gray-600 hover:text-indigo-700 cursor-pointer items-center mt-2">
+                        <div className="flex items-center">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-logout" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" />
+                            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+                            <path d="M7 12h14l-3 -3m0 6l3 -3" />
+                          </svg>
+                          <span className="text-sm ml-2">Sign out</span>
+                        </div>
+                      </li>
+                    </ul>
+                  ) : (
+                    ""
+                  )}
+                  <div className="relative">
+                    <img className="rounded-full h-10 w-10 object-cover" src="https://tuk-cdn.s3.amazonaws.com/assets/components/sidebar_layout/sl_1.png" alt="avatar" />
+                    <div className="w-2 h-2 rounded-full bg-green-400 border border-white absolute inset-0 mb-0 mr-0 m-auto" />
+                  </div>
+                </div>
+                <p className="text-gray-800 text-sm mx-3">Jane Doe</p>
+                <div className="cursor-pointer text-gray-600">
+                  <svg aria-haspopup="true" xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-down" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <polyline points="6 9 12 15 18 9" />
+                  </svg>
+                </div>
               </div>
             </div>
           </div>
@@ -26,7 +70,7 @@ export default function Navbar(props) {
               />
             </svg>
           </div>
-        </div>
+        {/* </div> */}
       </nav>
     </div>
   )
--- src/components/pages/Sidebar.js
+++ src/components/pages/Sidebar.js
@@ -6,19 +6,25 @@ export default function Sidebar(props) {
   const [productsecond, setProductsecond] = useState(false);
   const [productthird, setProductthird] = useState(false);
   return (
-    
+
     <div className={"w-full top-0 h-screen flex justify-between cursor-pointer " + (props.value ? "absolute translate-x-0 " : "hidden absolute -translate-x-full")}>
       <div className="inset-0 fixed left-0" onClick={props.sidebarHide} />
       <div className={`top-0 left-0 w-64 bg-gray-800 text-white shadow fixed h-full z-40 ease-in-out duration-300 ${props.value ? "relative translate-x-0 " : "relative translate-x-full"}`}>
-        <div className="flex text-6xl text-white justify-between cursor-pointer fixed right-5 top-7 z-50" onClick={props.sidebarHide}>
-          <span className="font-bold text-4xl ">PMSC</span>
-          <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
-          </svg>
+        <div className="flex items-center justify-between px-3">
+          <div className="h-16 w-full flex items-center justify-between">
+            <p className="text-6xl italic font-medium pl-5 text-indigo-700">PMSC</p>
+          </div>
+          <div id="closeSideBar" className="flex items-center justify-center h-10 w-10"onClick={props.sidebarHide}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-x" width={20} height={20} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" />
+              <line x1={18} y1={6} x2={6} y2={18} />
+              <line x1={6} y1={6} x2={18} y2={18} />
+            </svg>
+          </div>
         </div>
-        <div className="flex justify-center mt-20 h-4/5 text-center">
-          <div className="pt-2 fixed absolute">
-            <ul className="f-m-m h-96">
+        <div className="flex justify-center mt-5 text-center">
+          <div className="pt-2 fixed absolute h-48 max-h-full md:max-h-screen">
+            <ul className="f-m-m">
               <li className="flex flex-row items-center h-15 justify-center transform text-center hover:-translate-y-2 cursor-pointer transition-transform ease-in duration-200 text-white hover:bg-green-600 hover:text-white">
                 <span className="uppercase ml-1 text-2xl m-2 font-medium hover:italic" onClick={() => setProductfirst(!productfirst)}>Patients& therapsites</span>
               </li>
